21d4f261b120f10a2394d5502854fe8c44565c61
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2023-02-09 11:57:51 -0800

    Fix assertArrayMembersAreEqual bug

--- src/utilities/array-assertions.ts
+++ src/utilities/array-assertions.ts
@@ -2,6 +2,11 @@ export function assertArrayMembersAreEqual(
   arr1: Array<any>,
   arr2: Array<any>
 ): void {
+  if (arr1.length !== arr2.length) {
+    throw new Error(
+      `arrays have unequal length:\n\narr1: ${JSON.stringify(arr1)}\narr2: ${JSON.stringify(arr2)}\n`
+    )
+  }
   arr1.forEach((val, index) => {
     if (arr2[index] !== val) {
       throw new Error(
