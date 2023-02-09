e1154176e8efba3a493d3940dd3e5e0888bbdd3f
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2023-02-09 10:01:11 -0800

    (minor) Improve assertArrayMembersAreEqual output

--- src/utilities/array-assertions.ts
+++ src/utilities/array-assertions.ts
@@ -5,7 +5,7 @@ export function assertArrayMembersAreEqual(
   arr1.forEach((val, index) => {
     if (arr2[index] !== val) {
       throw new Error(
-        `arrays are not equal at index ${index}.\narr1[${index}]: ${arr1[index]}\narr2[${index}]: ${arr2[index]}`
+        `arrays are not equal at index ${index}.\n\narr1[${index}]: ${arr1[index]}\narr2[${index}]: ${arr2[index]}\n\narr1: ${JSON.stringify(arr1)}\narr2: ${JSON.stringify(arr2)}\n`
       );
     }
     return true;
